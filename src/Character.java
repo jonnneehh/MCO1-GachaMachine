0238f6931d2571234dc151ca8c437706985c9c53
jonnneehh <jon_llamado@dlsu.edu.ph>
2021-04-17 18:43:11 +0800

    Finished Character, Element and UI Class. Added Weapon Class.

---
+++ src/Character.java
@@ -0,0 +1,88 @@
+
+public class Character {
+	private String name;
+	private int rarity;
+	private Element element;
+	private int level;
+	//private Weapon weapon;
+	
+	public Character(String name, int rarity, Element element, int level) {
+		this.setName(name);
+		this.setRarity(rarity);
+		this.setElement(element);
+		this.setLevel(level);
+	}
+	
+	public int computeTotalCharInfluence() {
+		return this.level * (1 + ((this.rarity - 1) / 5));
+	}
+	
+	public float computeElementCombinationMultiplier(Character c) {
+		//Normal Pairing
+		if(this.element.getName() == c.element.getName())
+			return 1.1f;
+		
+		//Decent Pairing
+		else if((this.element.getName() == "Joker" && c.element.getName() == "Trigger") || 
+				(this.element.getName() == "Joker" && c.element.getName() == "Metal") || 
+				(this.element.getName() == "Metal" && c.element.getName() == "Cyclone") || 
+				(this.element.getName() == "Metal" && c.element.getName() == "Luna"))
+			return 1.25f;
+		
+		//Nice Pairing
+		else if((this.element.getName() == "Joker" && c.element.getName() == "Luna") ||
+				(this.element.getName() == "Joker" && c.element.getName() == "Heat"))
+			return 1.5f;
+		
+		//Perfect Pairing
+		else if((this.element.getName() == "Joker" && c.element.getName() == "Cyclone") || 
+				(this.element.getName() == "Trigger" && c.element.getName() == "Luna") ||
+				(this.element.getName() == "Metal" && c.element.getName() == "Heat"))
+			return 1.75f;
+		
+		//Bad Pairing
+		else if((this.element.getName() == "Cyclone" && c.element.getName() == "Luna") || 
+				(this.element.getName() == "Cyclone" && c.element.getName() == "Heat") ||
+				(this.element.getName() == "Luna" && c.element.getName() == "Heat"))
+			return 0.75f;
+		
+		//No effect
+		else
+			return 1.00f;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getRarity() {
+		return this.rarity;		
+	}
+	
+	public void setRarity(int rarity) {
+		this.rarity = rarity;
+	}
+	
+	public Element getElement() {
+		return this.element;
+	}
+	
+	public void setElement(Element element) {
+		this.element = element;
+	}
+	
+	public int getLevel() {
+		return this.level;
+	}
+	
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	
+	
+	
+}
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,4 +1,89 @@
+import java.util.Scanner;
 
 public class UserInterface {
-	//Test Test Test
+	private Scanner scanner = new Scanner(System.in);
+	
+	private int userChoice;
+	
+	public UserInterface() {
+		this.setUserChoice(0);
+	}
+	
+	public int getUserChoice() {
+		return this.userChoice;
+	}
+	
+	public void setUserChoice(int userChoice) {
+		this.userChoice = userChoice;
+	}
+	
+	public void startProgram() {
+		
+	}
+	
+	public int showMainMenu() {
+		System.out.println("Main Menu: ");
+		System.out.println("1: Show Characters");
+		System.out.println("2: Show Weapons");
+		System.out.println("3: Show Maps");
+		System.out.println("4: Show Gacha Machine");
+		System.out.println("5: Exit Program");
+		System.out.print("Enter your choice here: ");
+		this.userChoice = scanner.nextInt();
+		
+		return this.userChoice;
+	}
+	
+	public int showAllCharacters(/*Character characterList[]*/) {
+		System.out.println("Character Menu: ");
+		System.out.println("1: Level Up Characters");
+		System.out.println("2: Merge Two Character");
+		System.out.println("3: Equip Weapon to Character");
+		System.out.println("4: Back to Main Menu");
+		System.out.print("Enter your choice here: ");
+		this.userChoice = scanner.nextInt();
+		
+		return this.userChoice;
+	}
+	
+	public int showAllWeapons(/*Weapon weaponList[]*/) {
+		System.out.println("Weapon Menu: ");
+		System.out.println("1: Level Up Weapons");
+		System.out.println("2: Merge Two Weapons");
+		System.out.println("3: Equip Weapon to Character");
+		System.out.println("4: Back to Main Menu");
+		System.out.print("Enter your choice here: ");
+		this.userChoice = scanner.nextInt();
+		
+		return this.userChoice;
+	}
+	
+	public int showMaps(/*Map mapList[]*/) {
+		System.out.println("Map Menu: ");
+		System.out.println("1: Start Adventure");
+		System.out.println("2: Back to Main Menu");
+		System.out.print("Enter your choice here: ");
+		this.userChoice = scanner.nextInt();
+		
+		return this.userChoice;
+	}
+	
+	public int showGachaMachine(){
+		System.out.println("Gacha Machine: ");
+		System.out.println("1: Pull One Character (300)");
+		System.out.println("2: Pull Ten Characters (2700)");
+		System.out.println("3: Pull One Weapon (300)");
+		System.out.println("4: Pull Ten Weapons (2700)");
+		System.out.println("5: Back to Main Menu");
+		System.out.print("Enter your choice here: ");
+		this.userChoice = scanner.nextInt();
+		
+		return this.userChoice;
+	}
+	
+	public void exitProgram() {
+		System.out.println("Exiting Program...");
+		/* Some code to put here when the time comes.
+		 * */
+	}
 }
