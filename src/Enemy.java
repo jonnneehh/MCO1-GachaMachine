e7caa03b56cec8c6cbbde3c062f0093d0e7e39d1
miniwhyte <ysabel_manlapaz@dlsu.edu.ph>
2021-04-18 15:36:51 +0800

    updated map and enemy class

---
+++ src/Enemy.java
@@ -0,0 +1,29 @@
+
+public class Enemy {
+	//Variables
+	private String name;
+	private int power;
+		
+		
+	//Constructors
+	public Enemy(String name, int power) {
+		this.setName(name);
+		this.setPower(power);
+	}
+		
+		
+	//Getters and Setters
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPower() {
+		return this.power;
+	}
+	public void setPower(int power) {
+		this.power = power;
+	}	
+
+}
--- src/Map.java
+++ src/Map.java
@@ -1,8 +1,45 @@
 
 public class Map {
 	//Variables
+	private String name;
+	private int mapBaseAmount;
+	public Enemy[] enemyList;
+	
 	
 	//Constructors
+	public Map(String name, int mapBaseAmount, Enemy[] enemyList) {
+		this.setName(name);
+		this.setMapBaseAmount(mapBaseAmount);
+		this.setEnemyList(enemyList);
+	}
+	
+	
 	
 	//Methods
+	/*
+	 * public ..... computeCharacterSuperiority(one character)
+	 * public ..... computeCharacterSuperiority(two characters)
+	 * public int computeEnemySuperiority()
+	 * */
+	
+	//Getters and Setters
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMapBaseAmount() {
+		return this.mapBaseAmount;
+	}
+	public void setMapBaseAmount(int mapBaseAmount) {
+		this.mapBaseAmount = mapBaseAmount;
+	}
+	public Enemy[] getEnemyList() {
+		return this.enemyList;
+	}
+	public void setEnemyList(Enemy[] enemyList) {
+		this.enemyList = enemyList;
+	}
+	
 }
