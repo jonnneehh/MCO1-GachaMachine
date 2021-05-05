3e9918d80e6287e9e31d808a103d4e4d5db3079f
miniwhyte <ysabel_manlapaz@dlsu.edu.ph>
2021-05-05 21:09:50 +0800

    map updated

--- src/Map.java
+++ src/Map.java
@@ -1,31 +1,31 @@
+import java.util.ArrayList;
+
 
 public class Map {
 	//Variables
 	private String name;
 	private int mapBaseAmount;
-	private Enemy[] enemyList;
-	private int charInflu;
+	//private Enemy[] enemyList;
 	
 	
 	
 	//Constructors
-	public Map(String name, int mapBaseAmount, Enemy[] enemyList) {
+	public Map(String name, int mapBaseAmount, ArrayList<Integer> enemy) {
 		this.setName(name);
 		this.setMapBaseAmount(mapBaseAmount);
-		this.setEnemyList(enemyList);
 	}
 	
 	
 	//Methods
-	 public int computeCharacterSuperiority(Character chr) {
-		 return charInflu = chr.computeTotalCharInfluence();
+	 public int computeCharacterSuperiority(Character c) {
+		 return c.getWeapon().computeFinalWeaponPower() * (c.computeTotalCharInfluence() / 10);
 	 }
-	 public int computeCharacterSuperiority(two characters) {
-		 
+	 public int computeCharacterSuperiority(Character c1, Character c2) {
+		 return (c1.getWeapon().computeFinalWeaponPower() * (c1.computeTotalCharInfluence() / 10)) + (c2.getWeapon().computeFinalWeaponPower() * (c2.computeTotalCharInfluence() / 10));
+
 	 }
-	 public int computeEnemySuperiority(Enemy enemyList) {
-		 for()
-		 enemyList.getPower();
+	 public int computeEnemySuperiority(ArrayList<Enemy> enemy) {
+		 return 1;
 	 }
 	 
 	
@@ -42,11 +42,12 @@ public class Map {
 	public void setMapBaseAmount(int mapBaseAmount) {
 		this.mapBaseAmount = mapBaseAmount;
 	}
+	/*
 	public Enemy[] getEnemyList() {
 		return this.enemyList;
 	}
 	public void setEnemyList(Enemy[] enemyList) {
 		this.enemyList = enemyList;
 	}
-	
+	*/
 }
