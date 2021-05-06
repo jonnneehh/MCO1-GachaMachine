1f3243b1b1a1aa9b045d98f7efe6a363075fcf2d
miniwhyte <ysabel_manlapaz@dlsu.edu.ph>
2021-05-06 21:39:24 +0800

    added GachaMachine

---
+++ src/GachaMachine.java
@@ -0,0 +1,16 @@
+
+public class GachaMachine {
+	
+	public Character pullChar(int totalResources) {
+		totalResources = totalResources - 300;
+	}
+
+	public Weapon pullWeapon(int totalResources) {
+		totalResources = totalResources - 300;
+		
+	}
+	
+	
+	
+
+}
