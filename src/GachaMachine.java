7419e9baf35f10a2855671f48b398a4c43bf908c
miniwhyte <ysabel_manlapaz@dlsu.edu.ph>
2021-05-07 21:08:36 +0800

    added updates to GachaMachine

--- src/GachaMachine.java
+++ src/GachaMachine.java
@@ -1,22 +1,54 @@
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class GachaMachine {
 	
 	public Character pullChar(int totalResources) {
 		int rarity = 0;
+		int index = 0;
 		
 		totalResources = totalResources - 300;
 		rarity = rarityRandomizer();
 		
+		if(rarity == 1) {
+			//randomizes index
+			index = ThreadLocalRandom.current().nextInt(0, 5 + 1); //min, max + 1
+			
+			
+		}
+		else if(rarity == 2) {
+			index = ThreadLocalRandom.current().nextInt(6, 11 + 1);
+			
+		}
+		else {
+			index = ThreadLocalRandom.current().nextInt(12, 17 + 1);
+			
+		}
 		
 	}
 
 	public Weapon pullWeapon(int totalResources) {
 		int rarity = 0;
+		int index = 0;
 		
 		totalResources = totalResources - 300;
 		rarity = rarityRandomizer();
+		
+		if(rarity == 1) {
+			//randomizes index
+			index = ThreadLocalRandom.current().nextInt(0, 5 + 1); //min, max + 1
+			
+			
+		}
+		else if(rarity == 2) {
+			index = ThreadLocalRandom.current().nextInt(6, 11 + 1);
+			
+		}
+		else {
+			index = ThreadLocalRandom.current().nextInt(12, 17 + 1);
+			
+		}
 		
 		
 	}
