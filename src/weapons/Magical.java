fc5d2d93dcc244be25c9188c99ee7f73498c5d95
miniwhyte <ysabel_manlapaz@dlsu.edu.ph>
2021-05-22 20:50:59 +0800

    Added Magical Class

---
+++ src/weapons/Magical.java
@@ -0,0 +1,28 @@
+package weapons;
+
+public class Magical {
+	private int power;
+	private int calculatedRarity;
+	private int actualRarity;
+	
+	
+	public int getPower() {
+		return power;
+	}
+	public void setPower(int power) {
+		this.power = power;
+	}
+	public int getCalculatedRarity() {
+		return calculatedRarity;
+	}
+	public void setCalculatedRarity(int calculatedRarity) {
+		this.calculatedRarity = calculatedRarity;
+	}
+	public int getActualRarity() {
+		return actualRarity;
+	}
+	public void setActualRarity(int actualRarity) {
+		this.actualRarity = actualRarity;
+	}
+
+}
