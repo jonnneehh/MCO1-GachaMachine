23497408de9036a96bedc113a1a1d0283054dbdb
jonnneehh <jon_llamado@dlsu.edu.ph>
2021-05-22 21:02:56 +0800

    Added Bladed Class

---
+++ src/weapons/Bladed.java
@@ -0,0 +1,42 @@
+package weapons;
+
+public class Bladed extends Weapon{
+	private int power;
+	private int rarity;
+	private boolean isGolden;
+	
+	@Override
+	protected int computeFinalWeaponPower() { 
+		return this.power * this.rarity + (10 * this.rarity);
+	}
+	
+	@Override
+	protected int computeFinalWeaponRarity() {
+		return this.rarity;
+	}
+	
+	public int getPower() {
+		return power;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	public boolean isGolden() {
+		return isGolden;
+	}
+
+	public void setGolden(boolean isGolden) {
+		this.isGolden = isGolden;
+	}
+
+	public int getRarity() {
+		return rarity;
+	}
+
+	public void setRarity(int rarity) {
+		this.rarity = rarity;
+	}
+	
+}
